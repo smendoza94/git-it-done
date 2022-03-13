61218f8218c3fd16c48be6e1316bb56c9cdef657
Steven <stevenbmendoza@gmail.com>
2022-03-12 22:45:28 -0800

    git api fetch request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // format the gitHub api url
+    var apiURl = "https://api.github.com/users/" + user + "/repos";
+    // make a request to the url
+    fetch(apiURl).then(function(response) {
+        response.json().then(function(data){
+            console.log(data);
+        })
+    });
 };
 
-getUserRepos();
+getUserRepos("microsoft");
