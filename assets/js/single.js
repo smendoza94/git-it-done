36fa839dc9b844fd567fc4effaccae8642e87225
Steven <stevenbmendoza@gmail.com>
2022-03-13 18:44:14 -0700

    warning shows less than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,13 +2,19 @@ var issueContainerEl = document.querySelector("#issues-container");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    
+    var limitWarningEl = document.querySelector('#limit-warning')
+
     fetch(apiUrl).then(function(response) {
         //request was successful
         if (response.ok) {
             response.json().then(function(data){
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your requests!");
@@ -51,5 +57,18 @@ var displayIssues = function(issues) {
         issueContainerEl.append(issueEl);
     }  
 };
+
+displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target","_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
 
 getRepoIssues("facebook/.github");
